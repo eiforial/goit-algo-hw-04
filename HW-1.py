dd439ce1654426ac4ddb5fc7e2f372f1cf17b67e
eiforial <rabudal743@gmail.com>
2024-03-15 20:52:59 +0100

    HW-1.py

---
+++ HW-1.py
@@ -0,0 +1,23 @@
+file_path = "text.txt" 
+
+def total_salary(path):
+
+    try:
+        total = 0
+        count = 0
+
+        with open(path, 'r') as file:
+            for line in file:
+                _, salary = line.strip().split(',')
+                total += int(salary)
+                count += 1
+
+        average = total / count 
+        return total, average
+    
+    except FileNotFoundError:
+        print("Файл не найден.")
+        
+total, average = total_salary(file_path)
+
+print(f"Общая сумма зарплаты: {total} Средняя зарплата: {average}")
