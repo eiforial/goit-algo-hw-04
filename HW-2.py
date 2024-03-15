a5e9168175f0062ff73505f225ab1fbb8c0d2bd0
eiforial <rabudal743@gmail.com>
2024-03-15 23:18:30 +0100

    HW-2.py

---
+++ HW-2.py
@@ -0,0 +1,23 @@
+path = 'cats_file.txt'  
+
+def read_cats_info(file_path):
+    cats_list = []
+    try:
+        with open(path, 'r') as file:
+            for line in file:
+                cat_info = line.strip().split(',')
+                cat_dict = {
+                    "id": cat_info[0],
+                    "name": cat_info[1],
+                    "age": cat_info[2]
+                }
+                cats_list.append(cat_dict)
+    except FileNotFoundError:
+        print(f"Файл '{file_path}' не найден.")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+    return cats_list
+
+cats = read_cats_info(path)
+for cat in cats:
+    print(cat)
